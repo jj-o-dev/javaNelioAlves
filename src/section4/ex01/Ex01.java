25486394590ab0cfe9b61cee12feab424c2fa846
jj-o-devl <joaopauloresenderodrigues33@gmail.com>
2025-09-11 15:35:46 -0300

    DONE:section4/ex1,2,3

---
+++ src/section4/ex01/Ex01.java
@@ -0,0 +1,16 @@
+package section4.ex01;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        int z = x + y;
+
+        System.out.println(z);
+    }
+}
+
